6646b885becb1f7de4c5e4abc7dd537bc2f2a3e3
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-06-26 21:34:40 +0800

    Update twitter_download.py

--- modul/twitter_download.py
+++ modul/twitter_download.py
@@ -1,1 +1,35 @@
-#twitter
+import os
+from yt_dlp import YoutubeDL
+from colorama import init, Fore, Style
+from unduh import *
+
+init(autoreset=True)
+NAMA_FOLDER = "VidioDownload"
+#os.makedirs(NAMA_FOLDER, exist_ok=True)
+
+def unduh_twitter(alamat, cookies_path=None, resolusi=None):
+    try:
+        with YoutubeDL({'quiet': True}) as ydl:
+            info = ydl.extract_info(alamat, download=False)
+            judul = info.get('title', 'twitter_video')
+            ekstensi = info.get('ext', 'mp4')
+        tanggal = tanggal_hari_ini()
+        nama_file = cek_file_dan_konfirmasi(judul, ekstensi, tanggal)
+        if not nama_file:
+            return  # Batal
+        hasil_output = os.path.join(NAMA_FOLDER, nama_file)
+        opsi = {
+            'format': f"bestvideo[height<={resolusi}]+bestaudio/best[height<={resolusi}]" if resolusi else "bestvideo+bestaudio/best",
+            'outtmpl': hasil_output,
+            'noplaylist': True,
+            'quiet': False,
+            'progress_hooks': [yt_progress_hook],
+        }
+        if cookies_path:
+            opsi['cookiefile'] = cookies_path
+        with YoutubeDL(opsi) as ydl:
+            ydl.download([alamat])
+        print(Fore.GREEN + f"Download Twitter sukses. File: {hasil_output}")
+    except Exception as e:
+        print(Fore.RED + f"Download Twitter gagal! {e}")
+
