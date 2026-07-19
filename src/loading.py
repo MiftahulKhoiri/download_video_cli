48983942c27de8daf53c716319e47ee8c6e0c5a9
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-19 17:06:48 +0100

    Create loading.py

---
+++ src/loading.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def progress_hook(d):
+    if d["status"] == "downloading":
+        pct = d.get("_percent_str", "").strip()
+        speed = d.get("_speed_str", "").strip()
+        eta = d.get("_eta_str", "").strip()
+        filename = d.get("filename", "")
+        short_name = filename.split("/")[-1][:40]
+        sys.stdout.write(f"\r⬇️  {short_name} | {pct} @ {speed} ETA {eta}   ")
+        sys.stdout.flush()
+    elif d["status"] == "finished":
+        print("\n🔧 Memproses/menggabungkan file...")
+    elif d["status"] == "error":
+        print("\n❌ Terjadi error saat mengunduh.")
