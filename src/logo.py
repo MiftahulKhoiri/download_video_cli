fe408723104c4c28ed6f58043baa2835be376cf8
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-22 19:03:27 +0800

    Update logo.py

--- src/logo.py
+++ src/logo.py
@@ -1,11 +1,13 @@
+from colorama import init, Fore, Style
+init(autoreset=True)  # otomatis reset warna setelah setiap print
 
-def show_logo():
-    logo = r"""
- __   _____   ___          _
- \ \ / /_   ) |   \ _ __  | |
-  \ V / / /  | |) | '  \ | |__
-   \_/ /___| |___/|_|_|_||____|
+def show_logo_color():
+    print(Fore.CYAN + " __   _____   ___          _")
+    print(Fore.YELLOW + " \\ \\ / /_   ) |   \\ _ __  | |")
+    print(Fore.GREEN + "  \\ V / / /  | |) | '  \\ | |__")
+    print(Fore.MAGENTA + "   \\_/ /___| |___/|_|_|_||____|")
+    print()
+    print(Fore.RED + "🎬 " + Fore.BLUE + "YouTube" + Fore.WHITE + " / " + Fore.CYAN + "X" + Fore.WHITE + " Video & " + Fore.GREEN + "MP3 Downloader" + Fore.RED + " 🎵")
 
-  🎬 YouTube / X Video & MP3 Downloader 🎵
-    """
-    print(logo)
+if __name__ == "__main__":
+    show_logo_color()
