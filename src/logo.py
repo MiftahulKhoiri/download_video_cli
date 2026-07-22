d4a177d52df7650b4d67d2d3a8e6109e535adab7
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-22 18:56:18 +0800

    Create logo.py

---
+++ src/logo.py
@@ -0,0 +1,11 @@
+
+def show_logo():
+    logo = r"""
+ __   _____   ___          _
+ \ \ / /_   ) |   \ _ __  | |
+  \ V / / /  | |) | '  \ | |__
+   \_/ /___| |___/|_|_|_||____|
+
+  🎬 YouTube / X Video & MP3 Downloader 🎵
+    """
+    print(logo)
