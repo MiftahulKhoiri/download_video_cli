ac293c5f602fc57166aac522ea4e65c942520da4
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-09-24 22:04:18 +0800

    Update logo.py

--- src/logo.py
+++ src/logo.py
@@ -22,10 +22,10 @@ def show_logo():
 # ---------- Logo pembuka animasi: efek hujan digital (Matrix) ----------
 
 _LOGO_LINES = [
-    " __    __   __    __    {●}",
-    " \\ \ / /  /  \  /  \   {●}",
-    "  \\ V /  / /\ \/ /\ \  {●}",
-    "   \\_/  /_/  \__/  \_\ {●}",
+    r" __    __   __    __    {●}",
+    r" \ \ / /  /  \  /  \   {●}",
+    r"  \ V /  / /\ \/ /\ \  {●}",
+    r"   \_/  /_/  \__/  \_\ {●}",
 ]
 
 _TAGLINE = " Video & MP3 Downloader"
